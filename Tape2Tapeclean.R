8bcabeecf97de9b0d7f31573e40e4091bfb60f04
mcbarlowe <mcbarlowe@gmail.com>
2018-03-10 00:29:50 -0500

    Turned periodTime column to a cumsum for whole game

--- Tape2Tapeclean.R
+++ Tape2Tapeclean.R
@@ -1,6 +1,13 @@
 library(tidyverse)
 
-file_name <- c('')
+#convert player ids to actual player names from the player dataframe
+convert_ids <- function(column, player_df){
+    column <- player_df[match(column, player_df$playerId, nomatch = column), 
+                        c('fullName')]
+}
+
+file_name <- c('~/HockeyStuff/Tape2TapeData/11_10_plays.csv')
+players_file_name <- c('~/HockeyStuff/Tape2TapeData/11_10_roster.csv')
 
 pbp <- read_csv(file_name)
 
@@ -11,12 +18,12 @@ pbp$periodTime <- paste0('00:', pbp$periodTime)
 pbp$periodTime <- hms(pbp$periodTime)
 pbp$periodTime <- as.numeric(pbp$periodTime)
 
-#convert player ids to actual player names from the player dataframe
-convert_ids <- function(column, player_df){
-    column <- player_df[match(column, player_df$playerId, nomatch = column), 
-                        c('fullName')]
-}
+#convert periodTime to a cumsum of total seconds elapsed
+pbp$periodTime <- ifelse(pbp$period == 2, pbp$periodTime + 1200, pbp$periodTime)
+pbp$periodTime <- ifelse(pbp$period == 3, pbp$periodTime + 2400, pbp$periodTime)
+pbp$periodTime <- ifelse(pbp$period == 4, pbp$periodTime + 3600, pbp$periodTime)
 
-pbp[4:15] <- sapply(pbp[4:15], convert_ids, player_df = players)
+#converting playerids to playernames
+pbp[4:15] <- pbp %>% select(awayPlayer0Id:homePlayer5Id) %>%
+    sapply(convert_ids, player_df = players)
 
-pbp$awayPlayer0Id <- players[match(pbp$awayPlayer0Id, players$playerId), 4]
